cb5bc2c3db5b3499eeb30f3b9bc398f713e3c37a
Brenda Nascimento <162237685+Behguedes@users.noreply.github.com>
2025-05-29 09:48:27 -0300

    conceitos basicos de java

--- primeiroprojeto/src/Main.java
+++ primeiroprojeto/src/Main.java
@@ -1,6 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
+        System.out.println("Esse é o Screen Match!");
+        System.out.println("Filme: Rei leão");
 
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notadoFilme = 8.1f;
+        //Media calculada pelas 3 notas
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme Rei leão 
+                Filme de aventura sobre o reino de Mufasa.
+                Muito bom!
+                Ano de lancamento: """ + anoDeLancamento;
+        System.out.println(sinopse);
+
+        int classificacao = (int) (media / 2);
     }
 }
